625a04256e243edf48cd1a33fc0913fdd97771c7
lcastillo <lcastillo@finamiga.co>
2024-10-15 00:47:45 -0500

    sis-145-hu-00-t7: Unit test are created for the login process and user APIs

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from apps.users import views
 
 router = DefaultRouter()
-router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'users', views.UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
