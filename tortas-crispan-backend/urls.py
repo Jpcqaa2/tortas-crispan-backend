eef9404351c910d3e0158a07be991d054321cbc7
lcastillo <lcastillo@finamiga.co>
2024-11-04 18:41:00 -0500

    sis-170-hu-19: Configuración para despliegue del backend (APIs)

--- tortas-crispan-backend/urls.py
+++ tortas-crispan-backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for tortas_crispan_backend project.
+URL configuration for tortas-crispan-backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
